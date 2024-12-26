7dcc6f86edb5263470e2638a79e8eb93f419a162
Thiago Castro <thiago.castro@jftecnologia.local>
2024-12-26 09:11:07 -0300

    update: cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://192.168.105.29:5173', 'https://limppay-frontend-production.up.railway.app', 'https://admin-app-production-076f.up.railway.app', "https://limppay-api-production.up.railway.app", "https://admin-app-production-076f.up.railway.app", "http://192.168.1.41:5173"],
+    origin: ['http://localhost:5173', 'http://192.168.105.29:5173', 'https://limppay-frontend-production.up.railway.app', 'https://admin-app-production-076f.up.railway.app', "https://limppay-api-production.up.railway.app", "https://admin-app-production-076f.up.railway.app", "http://192.168.1.41:5173", "http://192.168.20.109:5173"],
     methods: [
       'GET',
       'POST',
